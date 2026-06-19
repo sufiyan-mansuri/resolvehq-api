59d7374562f150e3dcf08e6bf0add38533ba7994
Sufiyan Mansuri <mansurisufiyan141@gmail.com>
2026-06-19 16:55:41 +0530

    Changed url paths for api docs

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/resolvehq/docs/', permanent=True)),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('resolvehq-admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
     path('organizations/', include('apps.organizations.urls')),
     path('tickets/', include('apps.tickets.urls')),
     path('notifications/', include('apps.notifications.urls')),
-    path('resolvehq/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('resolvehq/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
